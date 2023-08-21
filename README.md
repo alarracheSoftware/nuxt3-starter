289701517b369a25a90702af9d55ef11b1164f59
felixleo22 <leofelixpro22@gmail.com>
2023-08-21 13:19:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Nuxt 3 Starter
+
+A frontend starter with nuxt3 and essential plugins.
+
+Plugins installed :
+- Vite (builder)
+- Vuetify (css)
+- Material design icons (icons)
+- i18n (internationalization)
+- Pinia (Store)
+- Axios (HTTP client)
+
+This starter has a default interface that includes :
+- App Bar
+- Open-close App Menu
+- Router
+- Dark theme
+- Reusable Snack work with Pinia
+
---
+++ components/skeleton/menu/LangSwitch.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-list v-model:opened="open">
+    <v-list-group value="Lang">
+      <template v-slot:activator="{ props }">
+        <v-list-item v-bind="props" title="Lang"></v-list-item>
+      </template>
+
+      <LangList />
+    </v-list-group>
+  </v-list>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+
+import LangList from '@/components/skeleton/menu/LangList.vue'
+
+const open = ref(['Lang']);
+
+</script>
+
+
